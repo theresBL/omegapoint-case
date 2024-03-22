37f5706d20470f75fe7015136eb14b7503f8b03e
theresBL <tbrannberglendt@gmail.com>
2024-03-22 14:49:27 +0100

    added navs and styling to header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,44 @@
+import { NavLink, Link } from 'react-router-dom'
 import styled from 'styled-components'
 import MovieCreationOutlinedIcon from '@mui/icons-material/MovieCreationOutlined'
-import { SearchBar } from './SearchBar'
+import SearchIcon from '@mui/icons-material/Search'
 
 export const Header = () => {
   return (
     <HeaderContainer>
       <HeaderContent>
         <IconWrapper>
-          <MovieCreationOutlinedIcon style={{ color: '#fff' }} />
+          <Link
+            type="button"
+            to="/"
+          >
+            <MovieCreationOutlinedIcon
+              style={{
+                color: '#fff'
+              }}
+              alt="Home button"
+            />
+          </Link>
         </IconWrapper>
-        <SearchBarWrapper>
-          <SearchBar />
-        </SearchBarWrapper>
+        <LinksWrapper>
+          <StyledNavLink
+            to="/mostRecent"
+          >
+            Most Recent
+          </StyledNavLink>
+          <StyledNavLink
+            to="/topRated"
+          >
+            Top Rated
+          </StyledNavLink>
+          <StyledNavLink
+            to="/search"
+          >
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+          </StyledNavLink>
+        </LinksWrapper>
       </HeaderContent>
     </HeaderContainer>
   )
@@ -19,6 +46,12 @@ export const Header = () => {
 
 const HeaderContainer = styled.div`
   display: flex;
+  position: fixed;
+  top: 0;
+  background-color: rgba(51, 51, 51, 0.9);
+  z-index: 100;
+  width: 100%;
+  height: 7rem;
 `
 
 const HeaderContent = styled.div`
@@ -49,7 +82,37 @@ svg {
 }
 `
 
-const SearchBarWrapper = styled.div`
-  display: flex;
-  align-items: center;
+const LinksWrapper = styled.div`
+display: flex;
+gap: 1rem;
+align-items: center;
+margin-right: 2rem;
+
+@media (min-width: 1024px) {
+    margin-right: 4rem;
+    gap: 3rem;
+  }
+`
+
+const StyledNavLink = styled(NavLink)`
+  color: #fff;
+  text-decoration: none;
+
+  &:hover {
+    filter: brightness(50%);
+  }
+
+  &.active {
+    text-decoration: underline;
+  }
+`
+
+const SearchIconWrapper = styled.div`
+ display: flex;
+
+ @media (min-width: 1024px) {
+  svg {
+    font-size: 2.5rem;
+  }
+  }
 `
