4bc6685f7772891686f4375295b91bc0720d02d0
theresBL <tbrannberglendt@gmail.com>
2024-03-24 21:26:35 +0100

    added content to homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,22 +1,133 @@
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { Page } from '../Components/ReusableStyles/Page'
 import { Loading } from '../Components/Loading'
 
 export const HomePage = () => {
+  const [mostRecentMovie, setMostRecentMovie] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+    const fetchMostRecentMovie = async () => {
+      try {
+        const response = await fetch(import.meta.env.VITE_API_URL)
+        const data = await response.json()
+
+        // Sort the movie list based on release date in descending order
+        data.results.sort((a, b) => new Date(b.release_date) - new Date(a.release_date))
+
+        // Extract the most recent movie (first element of the sorted array)
+        const mostRecent = data.results[0]
+        setMostRecentMovie(mostRecent)
+        setLoading(false)
+        console.log(mostRecent)
+      } catch (error) {
+        console.error('Error fetching most recent movie:', error)
+        setLoading(false)
+      }
+    }
+
+    fetchMostRecentMovie()
+  }, [])
+
+  const handleShowDetailsClick = () => {
+    if (mostRecentMovie) {
+      window.location.href = `/movieDetails/${mostRecentMovie.id}`
+    }
+  }
+
   return (
-    <Page>
-      <HomePageContainer>
-        <h1>Home Page</h1>
+    <HomePageContainer>
+      {loading ? (
         <Loading />
-      </HomePageContainer>
-    </Page>
+      ) : mostRecentMovie ? (
+        <div>
+          <PosterImage src={`https://image.tmdb.org/t/p/original${mostRecentMovie.poster_path}`} alt={mostRecentMovie.title} />
+          <HomeImage src={`https://image.tmdb.org/t/p/original${mostRecentMovie.backdrop_path}`} alt={mostRecentMovie.title} />
+          <div>
+            <HomeTitle>{mostRecentMovie.title}</HomeTitle>
+            <DetailsButton type="button" onClick={handleShowDetailsClick}>View details</DetailsButton>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </HomePageContainer>
   )
 }
 
 const HomePageContainer = styled.div`
-display: flex;
-width: 100vw;
-flex-direction: column;
-gap: 0.5rem;
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+const HomeTitle = styled.h1`
+  color: #fff;
+  position: absolute;
+  z-index: 11;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  @media (min-width: 1024px) {
+    top: 30%;
+    right: 10%;
+    transform: unset;
+    font-size: 5rem;
+  }
+`
+
+const PosterImage = styled.img`
+  height: 50%;
+  position: absolute;
+  z-index: 10;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  @media (min-width: 1024px) {
+    left: 20%;
+  }
   
+`
+
+const HomeImage = styled.img`
+  width: 100vw;
+  height: 100vh;
+  object-fit: center;
+  position: relative;
+  z-index: 0;
+  filter: brightness(70%);
+`
+
+const DetailsButton = styled.button`
+  background-color: red;
+  color: white;
+  border: none;
+  border-radius: 20px;
+  padding: 8px 10px;
+  font-size: 16px;
+  position: absolute;
+  top: 50%;
+  left: 70%;
+  z-index: 11;
+  transform: translate(-50%, -50%);
+
+  &:hover {
+    filter: brightness(50%);
+  }
+
+  @media (min-width: 900px) {
+    left: 65%;
+  }
+
+  @media (min-width: 1024px) {
+    font-size: 2rem;
+    left: 30%;
+  }
 `
